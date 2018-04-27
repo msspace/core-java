14bffa698e2b627d39d242818c8d9848abc8fc77
samantraym <samantraym@nero.gao.gov>
2018-04-27 16:18:06 -0400

    Added initial java design pattern examples.

---
+++ core-java/src/main/java/com/manas/core/oops/FirstClass.java
@@ -0,0 +1,14 @@
+package com.manas.core.oops;
+
+public class FirstClass {
+	public static void main(String args[]){
+		
+		//********************
+		 // Freshers 
+		//********************
+		//What is platform independence 
+		
+		
+	}
+
+}
